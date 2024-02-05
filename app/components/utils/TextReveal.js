ea9d6e8ee8256eeaff662706496226d1ea992ce3
euzenwebsites <152855764+euzenwebsites@users.noreply.github.com>
2024-02-05 19:22:22 +0500

    latest commit

--- app/components/utils/TextReveal.js
+++ app/components/utils/TextReveal.js
@@ -1,32 +1,32 @@
-"use client"
-import React, { Children, useEffect } from 'react'
-import gsap from 'gsap-trial'
-import { SplitText } from 'gsap-trial/all'
-import { ScrollTrigger } from 'gsap-trial/all'
+// "use client"
+// import React, { Children, useEffect } from 'react'
+// import gsap from 'gsap-trial'
+// import { SplitText } from 'gsap-trial/all'
+// import { ScrollTrigger } from 'gsap-trial/all'
 
-const TextReveal = ({Children}) => {
-  gsap.registerPlugin(SplitText,ScrollTrigger)
-  useEffect(()=>{
-    let mysplitText = new SplitText(".split",{type:'chars'})
-    let chars = mysplitText.chars
-    gsap.from(chars,{
-      yPercent:50,
-      stagger:0.03,
-      ease:"back.in",
-      duration:1.5,
-      scrollTrigger:{
-        trigger:".split",
-        start:"top 80%"
-      }
+// const TextReveal = ({Children}) => {
+//   gsap.registerPlugin(SplitText,ScrollTrigger)
+//   useEffect(()=>{
+//     let mysplitText = new SplitText(".split",{type:'chars'})
+//     let chars = mysplitText.chars
+//     gsap.from(chars,{
+//       yPercent:50,
+//       stagger:0.03,
+//       ease:"back.in",
+//       duration:1.5,
+//       scrollTrigger:{
+//         trigger:".split",
+//         start:"top 80%"
+//       }
   
-    }
+//     }
   
-    )
-  })
+//     )
+//   })
   
-  return (
-    <div className='split overflow-hidden'>{Children}</div>
-  )
-}
+//   return (
+//     <div className='split overflow-hidden'>{Children}</div>
+//   )
+// }
 
-export default TextReveal
+// export default TextReveal
