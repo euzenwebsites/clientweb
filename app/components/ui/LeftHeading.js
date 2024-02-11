2826e31083cdd2b5db555089c6e2caa083fc4354
euzenwebsites <152855764+euzenwebsites@users.noreply.github.com>
2024-02-11 15:31:12 +0500

    latest

--- app/components/ui/LeftHeading.js
+++ app/components/ui/LeftHeading.js
@@ -6,7 +6,7 @@ const LeftHeading = (props) => {
 
   return ( 
     <section className='w-[100%] flex justify-center mt-10' >
-      <div className="w-[90%] bg-[#dbdde2] py-6 sm:py-10 flex sm:justify-end border-b-8">
+      <div className="w-[90%] bg-[#dbdde2] py-4 xl:py-8 2xl:py-10 flex sm:justify-end border-b-8">
         <div className="w-[100%] sm:w-[50%] pr-10 sm:pr-40 p-4">
         <RevealSlide>
           <div >
